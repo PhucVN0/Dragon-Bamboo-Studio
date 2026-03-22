70ea8cdd2ca6b11ab86088152614d00664ef9f24
PhucVN0 <phucvn81@gmail.com>
2026-03-22 19:28:14 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,80 @@
+/* ================= LOADING ================= */
+
+window.addEventListener("load", () => {
+
+setTimeout(() => {
+
+document.getElementById("loader").style.display = "none";
+
+}, 1200);
+
+});
+
+/* ================= SCROLL BUTTON ================= */
+
+function scrollToProducts(){
+
+document.getElementById("products")
+.scrollIntoView({
+behavior:"smooth"
+});
+
+}
+
+/* ================= BUY FUNCTION ================= */
+
+function buyProduct(productName){
+
+showToast();
+
+setTimeout(()=>{
+
+const message =
+`Xin chào, tôi muốn mua ${productName}`;
+
+const discordLink =
+`https://discord.com/users/1379097098900869293`;
+
+window.open(discordLink,"_blank");
+
+},1500);
+
+}
+
+/* ================= TOAST ================= */
+
+function showToast(){
+
+const toast =
+document.getElementById("toast");
+
+toast.style.opacity = "1";
+
+setTimeout(()=>{
+
+toast.style.opacity = "0";
+
+},2000);
+
+}
+
+/* ================= SCROLL ANIMATION ================= */
+
+const observer =
+new IntersectionObserver((entries)=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+});
+
+document
+.querySelectorAll(".fade-in")
+.forEach(el=>observer.observe(el));
